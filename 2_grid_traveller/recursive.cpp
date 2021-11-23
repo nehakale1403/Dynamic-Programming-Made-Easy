8d6cbd5ad914a2d3844dc8bb0a7637f2f6df8115
Neha Kale <nkale8013@gmail.com>
2021-11-23 22:42:37 +0530

    grid traveller using recursion

---
+++ 2_grid_traveller/recursive.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gridTraveller(int m, int n){
+
+    if(m == 1 && n == 1)
+        return 1;
+    if(m <= 0 || n <= 0)
+        return 0;
+    
+    return gridTraveller(m-1, n) + gridTraveller(m, n-1);
+}
+
+int main(){
+    int m, n;
+    cin>>m>>n;
+
+    cout<<gridTraveller(m, n);
+}
