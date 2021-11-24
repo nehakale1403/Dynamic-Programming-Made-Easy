f8023729e63b2e0b2103cdf38fbf4385ab3ddb8a
Neha Kale <nkale8013@gmail.com>
2021-11-24 16:59:01 +0530

    can sum using recursion

---
+++ 3_canSum/recursive.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool canSum(int targetSum, int arr[], int n){
+
+    if(targetSum == 0)
+        return true;
+    if(targetSum < 0)
+        return false;
+    for(int i=0; i<n; i++){
+        
+        if(canSum(targetSum-arr[i], arr, n) == true){ 
+            return true; //if any of the sub problems return true, then the answer is true
+        }
+    }
+    return false;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+
+    int targetSum;
+    cin>>targetSum;
+
+    if(canSum(targetSum, arr, n))
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+
+    return 0;
+}
