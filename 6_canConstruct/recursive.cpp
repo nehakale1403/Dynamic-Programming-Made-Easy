ac8ce82f7e4d95ef90efec246687f5069698e0ee
Neha Kale <nkale8013@gmail.com>
2021-11-30 11:45:29 +0530

    can construct using recursion

--- 6_canConstruct/recursive.cpp
+++ 6_canConstruct/recursive.cpp
@@ -1,28 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool canConstruct(string target, vector<string> wordBank){
+string minusPrefix(string source, int len){
+
+    string retString = "";
+    for(int i=len; i<source.length(); i++){
+        retString = retString + source[i];
+    }
+
+    return retString;
+}
+
+bool canConstruct(string target,string wordBank[], int n){
     if(target == ""){
         return true;
     }
 
-    for(int i=0; i<wordBank.size(); i++){
-        
+    for(int i=0; i<n; i++){
+
+        //it checks if the given string is the substring of the target string, 
+        //if yes then it returns the starting index else returns -1
+        int pos = target.find(wordBank[i]);
+
+        if(pos == 0){
+            string newTarget = minusPrefix(target, wordBank[i].length());
+
+            if(canConstruct(newTarget, wordBank, n)){
+                return true;
+            }
+        }
     }
+    return false;
 }
 
 int main(){
     int n;
     cin>>n;
 
-    vector<string> wordBank(n);
+    string wordBank[n];
     for(int i=0; i<n; i++){
-        string s;
-        cin>>s;
-        wordBank.push_back(s);
+        cin>>wordBank[i];
     }
 
     string target;
     cin>>target;
-    canConstruct(target, wordBank);
+    if(canConstruct(target, wordBank, n))
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
 }
