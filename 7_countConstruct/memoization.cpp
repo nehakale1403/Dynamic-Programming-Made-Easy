faf7c7c837aef5ab2e8d10296db1ffcb8d0723d9
Neha Kale <nkale8013@gmail.com>
2021-11-30 17:29:44 +0530

    count construct using memoization

---
+++ 7_countConstruct/memoization.cpp
@@ -0,0 +1,51 @@
+//time complexity: O(n *m *m)
+//space complexity: O(m*m)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string minusString(string source, int len){
+
+    string temp = "";
+
+    for(int i=len; i<source.length(); i++)
+        temp += source[i];
+
+    return temp;
+}
+
+int countConstruct(int n, string wordBank[], string target, map<string, int> &memo){
+
+    int count = 0;
+
+    if(memo.find(target) != memo.end()) return memo[target];
+
+    if(target == "") return 1;
+
+    for(int i=0; i<n; i++){
+
+        int pos = target.find(wordBank[i]);
+        if(pos == 0){
+            string new_target = minusString(target, wordBank[i].length());
+        
+            count += countConstruct(n, wordBank, new_target, memo);
+        }
+    }
+    memo[target] = count;
+    return count;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    string wordBank[n];
+    for(int i=0; i<n; i++) cin>>wordBank[i];
+
+    string target;
+    cin>>target;
+
+    map<string, int> memo;
+
+    cout<<countConstruct(n, wordBank, target, memo);
+}
